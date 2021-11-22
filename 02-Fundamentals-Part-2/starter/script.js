632f7939c3068b78bd5df55d916dea5bc81ac106
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2021-11-22 00:36:49 -0800

    coding challenge #1 done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,84 @@
+//We should put this line very begining code all the time to avoid accidental error
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :)');
+
+// // const interface = 'Audio';
+// const private = 543;
+
+/*
+#1 Function 
+*/
+
+// function logger() {
+//     console.log('My name is Fumina')
+// }
+
+// // callig / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         return retirement;
+//         console.log()
+//     } else  {
+//         return -1;
+//     }
+    
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Fumina'));
+// console.log(yearsUntilRetirement(1950, 'Fumina'));
+
+/*
+Coding challenge #1
+*/
+
+const calcAverage = (a, b, c) => {
+    return (a + b + c) / 3;
+}
+
+//DATA1
+const avgDolhins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+//DATA2
+// const avgDolhins = calcAverage(85, 54, 41);
+// const avgKoalas = calcAverage(23, 34, 27);
+
+const checkWhinner = (avgDolhins, avgKoalas) => {
+
+    if (avgDolhins >= avgKoalas * 2) {
+        return console.log(`Dolhins win (${avgDolhins} vs ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolhins * 2) {
+        return console.log(`Koalas win (${avgKoalas} vs ${avgDolhins})`);
+    } else {
+        return console.log('No team win');
+    }
+
+}
+
+checkWhinner(avgDolhins, avgKoalas);
+
