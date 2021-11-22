3058fc6dcdaf81cece2b04b4901105fea18d7740
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2021-11-21 19:37:19 -0800

    data type lecture done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,36 @@
 /*
-#1 Assignments
+#1 Value and Variables Assignments
 */
 
-let country = 'Japan';
-let continent = 'Asia';
-let population = 125940852;
+// let country = 'Japan';
+// let continent = 'Asia';
+// let population = 125940852;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+/*
+#2 Data Types Practice
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1994;
+// console.log(typeof year);
+
+// console.log(typeof NULL);
+// // This shows 'Object' which is a bug of JS
